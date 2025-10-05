8dce36b46584fc61a740368ddf19d12d7f435cae
HugeCabbage69 <139630387+HugeCabbage69@users.noreply.github.com>
2025-10-05 11:48:47 +0530

    Trader menu
    made trader menu

--- Assets/Scripts/Player/PlayerValues.cs
+++ Assets/Scripts/Player/PlayerValues.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerValues : MonoBehaviour
 {
     public float health = 100f;
+    public float maxHealth = 100f;
     public int coins = 0;
 
     public float regenSpeed = 5f;
@@ -15,7 +16,7 @@ public class PlayerValues : MonoBehaviour
             health += regenSpeed * Time.deltaTime;
         }
 
-        health = Mathf.Clamp(health, 0f, 100f);
+        health = Mathf.Clamp(health, 0f, maxHealth);
         coins = Mathf.Clamp(coins, 0, 4);
     }
 }
---
+++ Assets/Scripts/Player/buy.cs
@@ -0,0 +1,49 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class buy : MonoBehaviour
+{
+    public PlayerValues playerValues;
+
+    public AttackingP patk;
+
+    public bool ishealth;
+    public bool isdash;
+    public FirstPersonMovement fpm;
+    public void OnBuyButtonClick()
+    {
+        if (playerValues != null)
+        {
+            if (playerValues.coins >= 2)
+            {
+                if (isdash)
+                {
+                    fpm.dashKey = KeyCode.LeftShift;
+                    playerValues.coins = playerValues.coins - 2;
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    if (ishealth)
+                    {
+                        playerValues.maxHealth = playerValues.maxHealth + 2;
+                        playerValues.coins = playerValues.coins - 2;
+                    }
+                    else
+                    {
+                        patk.damage = patk.damage + 2;
+                        playerValues.coins = playerValues.coins - 2;
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log("fucked");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("missig");
+        }
+    }
+}
